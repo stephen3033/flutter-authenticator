011479d28cd77445358474b3a50773882de0d663
Miller, Stephen Anthony <smiller336@gatech.edu>
2024-02-26 17:08:05 -0800

    initial setup with a stateless widget

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(auth());
+}
+
+class auth extends StatelessWidget {
+  const auth({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
